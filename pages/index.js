11d92809f9e89660c2bec4839afc45c241d9372d
BrazTh <thiagobraz0708@gmail.com>
2025-07-06 18:50:33 +0000

    Reforça a frase dizendo 4x

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
 function Home () {
-    return <h1> Tu é muito gay 🏳️‍🌈</h1>
+    return <h1> Tu é muito gay 🏳️‍🌈
+        Tu é muito gay 🏳️‍🌈
+        Tu é muito gay 🏳️‍🌈
+        Tu é muito gay 🏳️‍🌈
+    </h1>
 }
 
 export default Home;
