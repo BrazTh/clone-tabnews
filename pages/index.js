e32cdc48557edac7d4e2161b087df8f9b4f99397
BrazTh <thiagobraz0708@gmail.com>
2025-07-06 18:58:24 +0000

    Adiciona uma frase de carinho kkkkkk

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,12 @@
 function Home () {
-    return <h1> Tu é muito gay 🏳️‍🌈
+    return (
+    <h1> Tu é muito gay 🏳️‍🌈
         Tu é muito gay 🏳️‍🌈
         Tu é muito gay 🏳️‍🌈
         Tu é muito gay 🏳️‍🌈
+        Mas tu sabe que continua sendo meu irmão 👬
     </h1>
+    )
 }
 
 export default Home;
